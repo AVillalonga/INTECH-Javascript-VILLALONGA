fb3c1f45e05ff9f920501fe71da80ad08adc6cb9
AVillalonga <adrien.villalonga@gmail.com>
2021-11-30 18:09:38 +0100

    filter.js + zip.js

--- iterators/filter.js
+++ iterators/filter.js
@@ -1,22 +1,32 @@
+const assert = require('chai').assert;
+
+/**
+ * FILTER
+ * Implémenter un iterator filter(iterable, f) qui retournera uniquement les éléments de iterable qui passent 
+ * le test de la fonction f.
+ */
+
 function filter(iterable, f) {
+    const len = iterable.length
     let i = 0
-    let len = iterable.length
     return { 
         next : function(){
-            let result;
-            for (;i < len; i++){
-                if (f(iterable[i])) {
-                    result = {value:iterable[i], done:false}
-                    i++
-                    return result
-                }
+            let result = null;
+            while(i < len) {
+                result = f(iterable[i]) ? {value:iterable[i], done:false} : null;
+                i++;
+                if(result != null) return result;
             }
-            return { value: -1, done: true }
+            return { value: -1, done: true };
         }
     }
 }
 
+//********************************************************************************
+// TESTS
+//********************************************************************************
+
 const iterator = filter("hello", v => "aeiouy".includes(v));
-console.log(iterator.next().value) // => "e"
-console.log(iterator.next().value) // => "o"
-console.log(iterator.next().done) // => true
+assert.equal(iterator.next().value, 'e');
+assert.equal(iterator.next().value, 'o');
+assert.equal(iterator.next().done, true);
--- iterators/zip.js
+++ iterators/zip.js
@@ -1,13 +1,31 @@
+const assert = require('chai').assert;
+
+/**
+ * ZIP
+ * Implémenter un generator zip(...iterables) qui combine les valeurs de plusieurs iterables une à la fois.
+ * Quand l’un des iterable est vide, on arrête de lui demander des valeurs et on continue sur les iterables restants.
+ */
+
 function* zip(...iterables) {
-    const iterators = iterables.map(x => x[Symbol.iterator]());
-    while (true) {
-        const current = iterators.map(x => x.next());
-        if (current.some(x => x.done)) {
-            break;
-        }
-        yield current.map(x => x.value);
+    let iterators = iterables.map(iterable => iterable[Symbol.iterator]());
+    let result = [];
+
+    while(iterators.length) {
+        iteratorsValues = iterators.map(iterator => iterator.next());
+        iterators = iterators.filter((iterator, index) => !iteratorsValues[index].done);
+
+        iteratorsValues.forEach((interatorValue, index) => {
+            if(!iteratorsValues[index].done)
+                result.push(interatorValue.value)
+        });
     }
+
+    yield* result;
 }
 
-console.log([...zip("abc", [1, 2, 3])]);
-console.log([...zip("abcd", "1234", "+-=?")])
+//********************************************************************************
+// TESTS
+//********************************************************************************
+
+assert.deepEqual([...zip("abc", [1, 2, 3])], ['a', 1, 'b', 2, 'c', 3]);
+assert.deepEqual([...zip("abcd", "123", "+-")], ['a', '1', '+', 'b', '2', '-', 'c', '3', 'd']);
